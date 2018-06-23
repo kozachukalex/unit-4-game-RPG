2344665d8ca8ee037f3fceaefdc1023825951d8a
KamiKozy <kamikozytpp@gmail.com>
2018-06-23 17:53:14 -0400

    whooooa I'm 3/4s way there!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,196 @@
-$(document).ready(function () {
-
-    var windu = {
-        health: 30,
-        userDamage: 2
-    };
+$(function () {
+    var heroSelected = false;
+    var enemySelected = false;
     
-    fighterSelect = false;
-    enemySelect = false;
-
-    console.log(fighterSelect)
-    console.log(enemySelect)
-
-
-    if (fighterSelect === false) {
-        $(".fighter").on("click", function pickFighter() {
-
-            var ready = confirm("Do you want to fight as X ?");
-            if (ready === true) {
-                fighterSelect = true;
-                hero = $(this).attr("id")
-                console.log(hero);
-                $(this).remove(".fighter"),
-                $("#heroArea").append(this);   
-                console.log(fighterSelect);
-                return fighterSelect;
-            }
-  // this isn't returning out of the function that allows next section to be called
-        })
+    var windu = {
+        name: "Mace Windu",
+        health: 30,
+        damage: 1,
+        background: "council"
     }
-    console.log(fighterSelect);
-    if (fighterSelect === true) {
-        $("h2").html("Pick Your Opponent")
-        $(".fighter").on("click", function pickEnemy() {
-            enemy = $(this).attr("id")
-            console.log(enemy);
 
-            var ready = confirm("Do you want to fight against Y ?");
-            if (ready === true) {
-                enemySelect = true;
-                $("h2").html("Defeat Your Current Opponent Before Selecting Another!")
-                enemy = $(this).attr("id")
-                console.log(enemy);
-                $(this).remove(".fighter"),
-                $("#defenderArea").append(this);
-                console.log(enemySelect);
-                return enemySelect;
-            }
- // possible same issue as above once above is fixed
-        })
-
-
-
+    var yoda = {
+        name: "Yoda",
+        health: 40,
+        damage: 5,
+        background: "Dagobah"
     }
+
+    var darthMaul = {
+        name: "Darth Maul",
+        health: 20,
+        damage: 3,
+        background: "sand"
+    }
+
+    var vader = {
+        name: "Darth Vader",
+        health: 50,
+        damage: 6,
+        background: "Deathstar"
+    }
+
+    $('.fighter').on('click', function selectHero() {
+        if (heroSelected === true && enemySelected === true) {
+            alert("Defeat your opponent!");
+        }
+        if (heroSelected === true && enemySelected === false) {
+            enemySelected = true;
+            enemy = $(this).attr("id");
+            $(this).remove();
+            $('#defenderArea').append(this);
+            $('h2').html('Defeat Your Enemy!');
+            console.log(enemy)
+            if (enemy === "windu") {
+                $("#defenderArea").append("<div id = 'enemyHealth'>" + windu.health + "</div>")
+                $("#battleArena").css('background-image', 'url(assets/images/arenas/council.png)')
+            }
+            if (enemy === "yoda") {
+                $("#defenderArea").append("<div id = 'enemyHealth'>" + yoda.health + "</div>")
+                $("#battleArena").css('background-image', 'url(assets/images/arenas/dagobah.jpg)')
+            }
+            if (enemy === "darthMaul") {
+                $("#defenderArea").append("<div id = 'enemyHealth'>" + darthMaul.health + "</div>")
+                $("#battleArena").css('background-image', 'url(assets/images/arenas/sand.jpg)')
+            }
+            if (enemy === "vader") {
+                $("#defenderArea").append("<div id = 'enemyHealth'>" + vader.health + "</div>")
+                $("#battleArena").css('background-image', 'url(assets/images/arenas/deathstar.png)')
+            }
+        }
+        if (heroSelected === false) {
+            heroSelected = true;
+            hero = $(this).attr("id")
+            $(this).remove();
+            $('#heroArea').append(this);
+            $('h2').html('Pick Your Opponent');
+            console.log(hero)
+            if (hero === "windu") {
+                $("#heroArea").append("<div id = 'heroHealth'>" + windu.health + "</div>")
+            }
+            if (hero === "yoda") {
+                $("#heroArea").append("<div id = 'heroHealth'>" + yoda.health + "</div>")
+            }
+            if (hero === "darthMaul") {
+                $("#heroArea").append("<div id = 'heroHealth'>" + darthMaul.health + "</div>")
+            }
+            if (hero === "vader") {
+                $("#heroArea").append("<div id = 'heroHealth'>" + vader.health + "</div>")
+            }
+        }
+    })
+    $("#userAttack").on('click', function attack() {
+        // windo is hero
+        if (hero === "windu" & enemy === "yoda") {
+            windu.damage = 2 * windu.damage
+            yoda.health = yoda.health - windu.damage
+            windu.health = windu.health - yoda.damage
+            $("#heroHealth").html(windu.health)
+            $("#enemyHealth").html(yoda.health)
+        }
+        if (hero === "windu" & enemy === "darthMaul") {
+            windu.damage = 2 * windu.damage
+            darthMaul.health = darthMaul.health - windu.damage
+            windu.health = windu.health - darthMaul.damage
+            console.log(windu.damage)
+            $("#heroHealth").html(windu.health)
+            $("#enemyHealth").html(darthMaul.health)
+        }
+        if (hero === "windu" & enemy === "vader") {
+            windu.damage = 2 * windu.damage
+            vader.health = vader.health - windu.damage
+            windu.health = windu.health - vader.damage
+            console.log(windu.damage)
+            $("#heroHealth").html(windu.health)
+            $("#enemyHealth").html(vader.health)
+        }
+
+        //yoda is hero
+        if (hero === "yoda" & enemy === "windu") {
+            windu.health = windu.health - yoda.damage
+            windu.damage = 2 * windu.damage
+            yoda.health = yoda.health - windu.damage
+            $("#heroHealth").html(yoda.health)
+            $("#enemyHealth").html(windu.health)
+        }
+        if (hero === "yoda" & enemy === "darthMaul") {
+            darthMaul.health = darthMaul.health - yoda.damage
+            yoda.health = yoda.health - darthMaul.damage
+            $("#heroHealth").html(yoda.health)
+            $("#enemyHealth").html(darthMaul.health)
+        }
+        if (hero === "yoda" & enemy === "vader") {
+            vader.health = vader.health - yoda.damage
+            yoda.health = yoda.health - vader.damage
+            $("#heroHealth").html(yoda.health)
+            $("#enemyHealth").html(vader.health)
+        }
+
+        //darthMaul is hero
+        if (hero === "darthMaul" & enemy === "windu") {
+            windu.health = windu.health - darthMaul.damage
+            windu.damage = 2 * windu.damage
+            darthMaul.health = darthMaul.health - windu.damage
+            $("#heroHealth").html(darthMaul.health)
+            $("#enemyHealth").html(windu.health)
+        }
+        if (hero === "darthMaul" & enemy === "yoda") {
+            yoda.health = yoda.health - darthMaul.damage
+            darthMaul.health = darthMaul.health - yoda.damage
+            $("#heroHealth").html(darthMaul.health)
+            $("#enemyHealth").html(yoda.health)
+        }
+        if (hero === "darthMaul" & enemy === "vader") {
+            vader.health = vader.health - darthMaul.damage
+            darthMaul.health = darthMaul.health - vader.damage
+            $("#heroHealth").html(darthMaul.health)
+            $("#enemyHealth").html(vader.health)
+        }
+        
+        //vader is hero
+        if (hero === "vader" & enemy === "windu") {
+            windu.health = windu.health - vader.damage
+            windu.damage = 2 * windu.damage
+            vader.health = vader.health - windu.damage
+            $("#heroHealth").html(vader.health)
+            $("#enemyHealth").html(windu.health)
+        }
+        if (hero === "vader" & enemy === "yoda") {
+            yoda.health = yoda.health - vader.damage
+            vader.health = vader.health - yoda.damage
+            $("#heroHealth").html(vader.health)
+            $("#enemyHealth").html(yoda.health)
+        }
+        if (hero === "vader" & enemy === "darthMaul") {
+            darthMaul.health = darthMaul.health - vader.damage
+            vader.health = vader.health - darthMaul.damage
+            $("#heroHealth").html(vader.health)
+            $("#enemyHealth").html(darthMaul.health)
+        }
+    })
+
+    //when hp of current defender hits 0, remove picture
+
+    //allow a new opponent to be chosen
+
+    //move new opponent to defender
+
+    //when hp of 2nd defender hits 0, remove picture
+
+    //allow last opponent to be chosen
+
+    //move last opponent to defender
+
+    //when hp of last defender hits 0, remove picture and add victory
+
+    // if hero hp hits 0 at any point, game over
+
+
+
+
+
+
+
+
 
 })
